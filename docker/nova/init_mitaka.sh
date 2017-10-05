3c9c840fdba17412a4da1d7e8731ea882d6ed6b6
Toshiki Tsuboi <t.tsubo2000@gmail.com>
2017-10-05 10:09:16 +0900

    change keystone version from v2.0 to v3

--- docker/nova/init_mitaka.sh
+++ docker/nova/init_mitaka.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+mysql -uroot -pmysql123 -hmysql-openstack -P3306 -e "CREATE DATABASE nova_api;"
 mysql -uroot -pmysql123 -hmysql-openstack -P3306 -e "CREATE DATABASE nova;"
+mysql -uroot -pmysql123 -hmysql-openstack -P3306 -e "GRANT ALL PRIVILEGES ON nova_api.* TO 'nova'@'%' IDENTIFIED BY 'nova';"
 mysql -uroot -pmysql123 -hmysql-openstack -P3306 -e "GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'nova';"
 
 
+/usr/bin/nova-manage api_db sync
 /usr/bin/nova-manage db sync
 
-# Temporarally start glance for creating glance-image
+# Temporarally start nova for creating nova-flavor
 /usr/bin/nova-api --config-file=/etc/nova/nova.conf --log-file=/var/log/nova/nova-api.log &
 /usr/bin/nova-scheduler --config-file=/etc/nova/nova.conf --log-file=/var/log/nova/nova-scheduler.log &
 /usr/bin/nova-novncproxy --config-file=/etc/nova/nova.conf --log-file=/var/log/nova/nova-novncproxy.log &
@@ -14,10 +17,10 @@ mysql -uroot -pmysql123 -hmysql-openstack -P3306 -e "GRANT ALL PRIVILEGES ON nov
 /usr/bin/nova-consoleauth --config-file=/etc/nova/nova.conf --log-file=/var/log/nova/nova-consoleauth.log &
 /usr/bin/nova-console --config-file=/etc/nova/nova.conf --log-file=/var/log/nova/nova-console.log &
 /usr/bin/nova-compute --config-file=/etc/nova/nova.conf --log-file=/var/log/nova/nova-compute.log &
-sleep 3
+sleep 10
 
-openstack flavor create m1.tiny \
-  --id 1 \
+openstack flavor create m1.sample \
+  --id 6 \
   --ram 512 \
   --disk 1 \
   --vcpus 1
