a54027a3ff38e0cbbfd929f7ec3886666c114de1
Toshiki Tsuboi <t.tsubo2000@gmail.com>
2017-10-06 12:43:50 +0900

    dockerized neutron

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 $ docker build -t ttsubo/keystone:1.0.0 docker/keystone/.
 $ docker build -t ttsubo/glance:1.0.0 docker/glance/.
 $ docker build -t ttsubo/nova:1.0.0 docker/nova/.
+$ docker build -t ttsubo/neutron:1.0.0 docker/neutron/.
 ```
---
+++ docker/neutron/init_mitaka.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mysql -uroot -pmysql123 -hmysql-openstack -P3306 -e "CREATE DATABASE neutron;"
+mysql -uroot -pmysql123 -hmysql-openstack -P3306 -e "GRANT ALL PRIVILEGES ON neutron.* TO 'nova'@'%' IDENTIFIED BY 'neutron';"
+
+
+/usr/bin/neutron-db-manage --config-file /etc/neutron/neutron.conf \
+  --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head
+
+# Temporarally start neutron for creating network-resource
+sleep 10
+/usr/bin/neutron-server --config-file=/etc/neutron/neutron.conf --log-file=/var/log/neutron/neutron-server.log &
+/usr/bin/neutron-linuxbridge-agent --config-file=/etc/neutron/neutron.conf --log-file=/var/log/neutron/neutron-linuxbridge-agent.log &
+/usr/bin/neutron-dhcp-agent --config-file=/etc/neutron/neutron.conf --log-file=/var/log/neutron/neutron-dhcp-agent.log &
+/usr/bin/neutron-l3-agent --config-file=/etc/neutron/neutron.conf --log-file=/var/log/neutron/neutron-l3-agent.log &
+sleep 10
+
+
+openstack network create admin_net
+neutron subnet-create admin_net 10.0.0.0/24 --name admin_subnet
+
+echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf 
+echo 'net.ipv4.conf.default.rp_filter=0' >> /etc/sysctl.conf 
+echo 'net.ipv4.conf.all.rp_filter=0' >> /etc/sysctl.conf 
+sysctl -p 
+
+
+pkill neutron-server
+pkill neutron-linuxbridge-agent
+pkill neutron-dhcp-agent
+pkill neutron-l3-agent
